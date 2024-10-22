4b6986faeae87cd843e71920e973e7322d71c863
Sandrine Poissonnet <spoisson@gmail.com>
2024-10-22 11:32:43 -0700

    Add badges on Product ca rds

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,9 @@
 import { products } from "@wix/stores";
 import Link from "next/link";
 import WixImage from "./WixImage";
-
+import Badge from "./ui/badge-custom";
+import { formatCurrency } from "@/lib/utils";
+import DiscountBadge from "./DiscountBadge";
 interface ProductProps {
   product: products.Product;
 }
@@ -11,7 +13,7 @@ export default function Product({ product }: ProductProps) {
 
   return (
     <Link href={`/products/${product.slug}`} className="h-full border bg-card">
-      <div className="overflow-hidden">
+      <div className="relative overflow-hidden">
         <WixImage
           mediaIdentifier={mainImage?.url}
           alt={mainImage?.altText}
@@ -19,6 +21,13 @@ export default function Product({ product }: ProductProps) {
           width={700}
           height={700}
         />
+        <div className="absolute bottom-3 right-3 flex flex-wrap items-center gap-2">
+          {product.ribbon && <Badge>{product.ribbon}</Badge>}
+          {product.discount && <DiscountBadge data={product.discount} />}
+          <Badge className="bg-secondary font-semibold text-secondary-foreground">
+            {getFormattedPrice(product)}
+          </Badge>
+        </div>
       </div>
       <div className="space-y-3 p-3">
         <h3 className="text-lg font-bold">{product.name}</h3>
@@ -29,4 +38,19 @@ export default function Product({ product }: ProductProps) {
       </div>
     </Link>
   );
+}
+
+function getFormattedPrice(product: products.Product) {
+  const minPrice = product.priceRange?.minValue;
+  const maxPrice = product.priceRange?.maxValue;
+
+  if (minPrice && maxPrice && minPrice !== maxPrice) {
+    return `from ${formatCurrency(minPrice, product.priceData?.currency)}`;
+  } else {
+    return (
+      product.priceData?.formatted?.discountedPrice ||
+      product.priceData?.formatted?.price ||
+      "n/a"
+    );
+  }
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(
+  price: number | string = 0,
+  currency: string = "USD",
+) {
+  return Intl.NumberFormat("en", { style: "currency", currency }).format(
+    Number(price),
+  );
 }
