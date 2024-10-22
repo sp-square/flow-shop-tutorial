9d710838c2a2e63fc02b089d5a76998d6930ea7a
Sandrine Poissonnet <spoisson@gmail.com>
2024-10-22 10:20:39 -0700

    Refactor Product.tsx and create a reusable WixImage.tsx component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable @next/next/no-img-element */
 import { products } from "@wix/stores";
 import Link from "next/link";
-import { media as wixMedia } from "@wix/sdk";
+import WixImage from "./WixImage";
 
 interface ProductProps {
   product: products.Product;
@@ -10,18 +9,15 @@ interface ProductProps {
 export default function Product({ product }: ProductProps) {
   const mainImage = product.media?.mainMedia?.image;
 
-  // on Wix you can resize an image for free - on Vercel it would cost the compute power for resizing
-  const resizedImageUrl = mainImage?.url
-    ? wixMedia.getScaledToFillImageUrl(mainImage.url, 700, 700, {})
-    : null;
-
   return (
     <Link href={`/products/${product.slug}`} className="h-full border bg-card">
       <div className="overflow-hidden">
-        <img
-          src={resizedImageUrl || "/placeholder.png"}
-          alt={mainImage?.altText || ""}
+        <WixImage
+          mediaIdentifier={mainImage?.url}
+          alt={mainImage?.altText}
           className="transition-transform duration-300 hover:scale-105"
+          width={700}
+          height={700}
         />
       </div>
       <div className="space-y-3 p-3">
---
+++ src/components/WixImage.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
+// on Wix you can resize an image for free - on Vercel it would cost the compute power for resizing
+
+import { media as wixMedia } from "@wix/sdk";
+import { ImgHTMLAttributes } from "react";
+
+type WixImageProps = Omit<
+  ImgHTMLAttributes<HTMLImageElement>,
+  "src" | "width" | "height" | "alt"
+> & {
+  mediaIdentifier: string | undefined;
+  placeholder?: string;
+  alt?: string | null | undefined;
+} & (
+    | {
+        scaleToFill?: true;
+        width: number;
+        height: number;
+      }
+    | {
+        scaleToFill: false;
+      }
+  );
+
+export default function WixImage({
+  mediaIdentifier,
+  placeholder = "/placeholder.png",
+  alt,
+  ...props
+}: WixImageProps) {
+  const imageUrl = mediaIdentifier
+    ? props.scaleToFill || props.scaleToFill === undefined
+      ? wixMedia.getScaledToFillImageUrl(
+          mediaIdentifier,
+          props.width,
+          props.height,
+          {},
+        )
+      : wixMedia.getImageUrl(mediaIdentifier).url
+    : placeholder;
+
+  return <img src={imageUrl} alt={alt || ""} {...props} />;
+}
