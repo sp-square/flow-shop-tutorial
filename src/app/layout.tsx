04fa054c026574c920cecf3e23db6ce474c72704
Sandrine Poissonnet <spoisson@gmail.com>
2024-10-23 14:30:55 -0700

    basic footer added and terms and conditions page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Lora } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const lora = Lora({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={lora.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
