e093496517364502bc2b51866cd0de43c58c719d
Sandrine Poissonnet <spoisson@gmail.com>
2024-10-23 15:00:25 -0700

    create a wix-api layer a nd refactor Navbar and the Home page.tsx accordingly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import banner from "@/assets/banner.jpg";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import { getWixClient } from "@/lib/wix-client.base";
 import { Suspense } from "react";
 import Product from "@/components/Product";
 import { Skeleton } from "@/components/ui/skeleton";
+import { getCollectionBySlug } from "@/wix-api/collections";
+import { queryProducts } from "@/wix-api/products";
 
 export default function Home() {
   return (
@@ -43,20 +44,15 @@ export default function Home() {
 }
 
 async function FeaturedProducts() {
-  const wixClient = getWixClient();
-
-  const { collection } =
-    await wixClient.collections.getCollectionBySlug("featured-products");
+  const collection = await getCollectionBySlug("featured-products");
 
   if (!collection?._id) {
     return null;
   }
 
-  const featuredProducts = await wixClient.products
-    .queryProducts()
-    .hasSome("collectionIds", [collection._id])
-    .descending("lastUpdated")
-    .find();
+  const featuredProducts = await queryProducts({
+    collectionIds: collection._id,
+  });
 
   if (!featuredProducts.items.length) {
     return null;
